124e5a9fc6f098b2a7ea659fe34b7a9ff1a8573c
HabibeC <habibecelikten@gmail.com>
2023-03-14 23:00:51 +0300

    new components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { MainCountComponent } from './components/ngrx/main-count/main-count.comp
 import { SelectionComponent } from './components/selection/selection.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/e-ticaret/home/home.component';
-import { LayoutsComponent } from './components/e-ticaret/layouts/layouts.component';
+import { HomeComponent } from './components/e-commerce/home/home.component';
+import { LayoutsComponent } from './components/e-commerce/layouts/layouts.component';
 
 const routes: Routes = [
   {
